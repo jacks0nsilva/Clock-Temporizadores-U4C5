dc63614d8101d2890c3e1571afd0ddac6f67ec84
Jackson Silva <jacksonsilva_fsa@outlook.com>
2025-02-03 20:05:57 -0300

    Implementação da temporização por hardware

--- Atividade1/Atividade1.c
+++ Atividade1/Atividade1.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 
-
+bool repeating_timer_callback(struct repeating_timer *timer) {
+    printf("Callback is on!\n");
+    return true;
+}
 
 int main()
 {
+    // Inicializa a comunicação serial
     stdio_init_all();
 
+    // Declaração da estrutura do temporizador
+    // Essa estrutura armzena informações sobre o temporizador
+    struct repeating_timer timer;
+
+    add_repeating_timer_ms(1000, repeating_timer_callback, NULL, &timer);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        sleep_ms(10); // Pausa por 10 milissegundos para não sobrecarregar o processador
     }
 }
